3c64266b0bba2f249434e432f2250e3d84d5c27c
desktop <psomail@yandex.ru>
2024-02-12 22:32:43 +0300

    task10 init

---
+++ task10/data — копия.js
@@ -0,0 +1,543 @@
+const dataInfo = [
+	{
+		"filmId": 1040584,
+		"nameRu": "СССР",
+		"type": "TV_SERIES",
+		"year": "2020",
+		"description": "Весна 1981 года. Олимпиада позади, но еще не стерлась со стен олимпийская символика. Спокойное, мирное, сонное время – брежневский застой. Четверо друзей возвращаются из армии, вместе они прошли огонь и воду, нештатные ситуации, спасали друг другу жизнь. В шутку они называют себя «СССР» по первым буквам имен – Сергей, Семен, Самвел и Рома. История дружбы и взросления развивается на фоне стремительно меняющейся страны, но у их «СССР», в отличие от Советского Союза, есть надежда. Союз друзей сохранится, хотя за десять лет не раз будет испытан на прочность.",
+		"countries": [
+			{
+				"country": "Россия"
+			}
+		],
+		"genres": [
+			{
+				"genre": "мелодрама"
+			}
+		],
+		"rating": "7.7",
+		"ratingVoteCount": 42070,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1040584.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1040584.jpg"
+	},
+	{
+		"filmId": 1365214,
+		"nameEn": "SS",
+		"type": "FILM",
+		"year": "2020",
+		"filmLength": "00:10",
+		"countries": [
+			{
+				"country": "США"
+			}
+		],
+		"genres": [
+			{
+				"genre": "короткометражка"
+			}
+		],
+		"rating": "null",
+		"ratingVoteCount": 3,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1365214.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1365214.jpg"
+	},
+	{
+		"filmId": 437392,
+		"nameRu": "Ледяной цветок",
+		"nameEn": "Ssanghwajeom",
+		"type": "FILM",
+		"year": "2008",
+		"description": "Монарх порабощенного Юаньской империей государства Корё выбрал и воспитал мальчиков для личной стражи, названной Конрёнве. С позволения короля стражники жили во дворце и всегда охраняли своего господина. Глава стражи Хон-рим пользовался особой благосклонностью короля, чем вызывал у королевы обиду и ревность. Тем временем отношения между Корё и Юань обострились. \n\nИмператор Юань под предлогом отсутствия у трона Корё законного наследника потребовал назначить наследным принцем двоюродного брата короля - Кёнвона. Король, невзирая на недовольство высокопоставленных чиновников, отверг это требование. Будучи неспособным на соитие с женщиной, он втайне повелел своему любовнику Хон-риму спать с королевой, дабы та смогла зачать дитя, которое позволило бы королю сохранить власть.",
+		"filmLength": "02:12",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "драма"
+			},
+			{
+				"genre": "история"
+			},
+			{
+				"genre": "мелодрама"
+			}
+		],
+		"rating": "7.8",
+		"ratingVoteCount": 9344,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/437392.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/437392.jpg"
+	},
+	{
+		"filmId": 1043837,
+		"nameRu": "Прорвёмся",
+		"nameEn": "Ssam, maiwei",
+		"type": "TV_SERIES",
+		"year": "2017",
+		"description": "Когда-то в школьные годы Ко Дон-ман занимался тхэквондо и грезил о профессиональном спорте. Сейчас же парень зарабатывает на жизнь участием в боях без правил. Чхве Э-ра надеялась стать диктором на телевидении, но вместо этого работает за информационной стойкой в универмаге. Эта история о том, что судьба часто складывается не так, как того желаешь, однако выбор — сдаваться или сражаться — всегда за тобой.",
+		"filmLength": "01:00",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "комедия"
+			},
+			{
+				"genre": "мелодрама"
+			}
+		],
+		"rating": "8.0",
+		"ratingVoteCount": 17560,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1043837.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1043837.jpg"
+	},
+	{
+		"filmId": 989091,
+		"nameRu": "Британские СС",
+		"nameEn": "SS-GB",
+		"type": "MINI_SERIES",
+		"year": "2017",
+		"description": "Ноябрь 1941. Лондон. Британия оккупирована фашистами. Дуглас Арчер, известный в Лондоне сыщик, занимается расследованием убийств в Скотланд-Ярде. Хотя теперь начальство у Скотланд-Ярда другое, Дугласа угрызения совести не мучают — кто-то же должен ловить убийц, а в политику он не лезет. Но вот однажды утром Дуглас начинает расследовать одно загадочное убийство, которое выведет его и к Британскому Сопротивлению, и к разведкам разных держав, и в итоге — к очень большой политике.",
+		"filmLength": "01:00",
+		"countries": [
+			{
+				"country": "Великобритания"
+			}
+		],
+		"genres": [
+			{
+				"genre": "боевик"
+			},
+			{
+				"genre": "драма"
+			},
+			{
+				"genre": "триллер"
+			},
+			{
+				"genre": "фантастика"
+			}
+		],
+		"rating": "6.3",
+		"ratingVoteCount": 698,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/989091.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/989091.jpg"
+	},
+	{
+		"filmId": 906852,
+		"nameRu": "СС",
+		"nameEn": "Die SS - Eine Warnung der Geschichte",
+		"type": "TV_SERIES",
+		"year": "2002",
+		"description": "Они уверены: будущее принадлежит им. Они одержимы собой и магией огня. Адепты огненного культа. СС. Гидра с множеством лиц. Они были синонимом третьего рейха, кодовым обозначением гитлеровской Германии, нацистской Германии, ужасов и смерти, которая могла ударить в любой момент. Сегодня они уверяют, что были солдатами, как и все остальные. Во время войны войска СС были передовыми ударными подразделениями. Беспринципными и абсолютно послушными. Мужчины, научившиеся принимать смерть и нести смерть. Не считающиеся с потерями. Верные солдаты Гитлера, а во главе - полицейский. Элитные солдаты - вот, кем они хотели стать. Хотя зачастую они были преступниками. Но в верхах об этом не упоминалось, в верхах имело значение лишь убийство миллионов...",
+		"filmLength": "00:43",
+		"countries": [
+			{
+				"country": "Германия"
+			}
+		],
+		"genres": [
+			{
+				"genre": "документальный"
+			}
+		],
+		"rating": "null",
+		"ratingVoteCount": 19,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/906852.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/906852.jpg"
+	},
+	{
+		"filmId": 1338307,
+		"nameRu": "Поиск",
+		"nameEn": "Sseochi",
+		"type": "TV_SERIES",
+		"year": "2020",
+		"description": "Военно-поисковый отряд в демилитаризованной зоне противостоит неведомому противнику.",
+		"filmLength": "01:00",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "боевик"
+			},
+			{
+				"genre": "триллер"
+			}
+		],
+		"rating": "7.6",
+		"ratingVoteCount": 2938,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1338307.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1338307.jpg"
+	},
+	{
+		"filmId": 1297212,
+		"nameRu": "Бар на колёсах",
+		"nameEn": "Ssanggappocha",
+		"type": "TV_SERIES",
+		"year": "2020",
+		"description": "Загадочный бар под вывеской «Twin» материализуется в полночь. Туда захаживают как живые, так и давно почившие личности - общаясь и выпивая с хозяйкой Воль-джу, они залечивают свои душевные раны.",
+		"filmLength": "01:10",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "драма"
+			},
+			{
+				"genre": "комедия"
+			},
+			{
+				"genre": "мелодрама"
+			},
+			{
+				"genre": "фэнтези"
+			}
+		],
+		"rating": "7.9",
+		"ratingVoteCount": 2358,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1297212.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1297212.jpg"
+	},
+	{
+		"filmId": 1272433,
+		"nameRu": "Дневник психопата",
+		"nameEn": "Ssaikopaeseu daieori",
+		"type": "TV_SERIES",
+		"year": "2019",
+		"description": "К скромному служащему брокерской конторы Юк Тон-щику в руки случайно попадает дневник серийного убийцы, где подробно описаны его преступления. После этого парень из-за несчастного случая теряет память и решает, что он сам и есть маньяк. С этого момента Тон-щик начинает меняться. Полицейская Щим Бо-гён решает использовать эту возможность, чтобы поймать настоящего убийцу.",
+		"filmLength": "01:20",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "детектив"
+			},
+			{
+				"genre": "комедия"
+			},
+			{
+				"genre": "криминал"
+			},
+			{
+				"genre": "триллер"
+			}
+		],
+		"rating": "7.8",
+		"ratingVoteCount": 9029,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1272433.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1272433.jpg"
+	},
+	{
+		"filmId": 5147309,
+		"nameEn": "SS Rajamouli's #SSMB29",
+		"type": "FILM",
+		"year": "null",
+		"countries": [
+			{
+				"country": "Индия"
+			}
+		],
+		"genres": [
+			{
+				"genre": "боевик"
+			},
+			{
+				"genre": "приключения"
+			},
+			{
+				"genre": "триллер"
+			}
+		],
+		"rating": "null",
+		"ratingVoteCount": 0,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/5147309.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/5147309.jpg"
+	},
+	{
+		"filmId": 570147,
+		"nameRu": "Назад в СССР",
+		"type": "MINI_SERIES",
+		"year": "2010",
+		"description": "Преуспевающий бизнесмен Антон переживает кризис среднего возраста. Он много выпивает и по совету близкого друга начинает посещать общество анонимных алкоголиков. Там мужчина знакомится с человеком, называющим себя Сталкером, который предлагает приключение и ощущения, которых он никогда не испытывал. Антон соглашается и внезапно понимает, что оказался в 1975 году.",
+		"filmLength": "00:47",
+		"countries": [
+			{
+				"country": "Россия"
+			}
+		],
+		"genres": [
+			{
+				"genre": "мелодрама"
+			},
+			{
+				"genre": "фантастика"
+			}
+		],
+		"rating": "6.8",
+		"ratingVoteCount": 12063,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/570147.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/570147.jpg"
+	},
+	{
+		"filmId": 984253,
+		"nameRu": "Давай сражаться, призрак",
+		"nameEn": "Ssauja gwisina",
+		"type": "TV_SERIES",
+		"year": "2016",
+		"description": "Пак обладает сверхъестественной способностью не только видеть призраков, но и изгонять их. Поэтому с удовольствием оказывает помощь тем, кто нуждается в услугах экзорциста. Но однажды он сталкивается с призраком школьницы, и во время исполнения обряда случайно целует её, да так, что искры летят.",
+		"filmLength": "01:00",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "комедия"
+			},
+			{
+				"genre": "мелодрама"
+			},
+			{
+				"genre": "фэнтези"
+			}
+		],
+		"rating": "7.8",
+		"ratingVoteCount": 18506,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/984253.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/984253.jpg"
+	},
+	{
+		"filmId": 996611,
+		"nameRu": "Демон",
+		"nameEn": "Dokkaebi",
+		"type": "TV_SERIES",
+		"year": "2016",
+		"description": "Бессмертный демон токкэби много лет живет среди смертных и порядком устал от жизни. Но если ты волшебное существо, есть лишь один способ поставить точку и покинуть бренный мир — жениться на смертной. Избранницей демона становится девушка, которая может видеть призраков. А ангел смерти, чья задача провожать души умерших в загробный мир, тем временем потерял память.",
+		"filmLength": "01:15",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "драма"
+			},
+			{
+				"genre": "комедия"
+			},
+			{
+				"genre": "мелодрама"
+			},
+			{
+				"genre": "фэнтези"
+			}
+		],
+		"rating": "8.5",
+		"ratingVoteCount": 150172,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/996611.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/996611.jpg"
+	},
+	{
+		"filmId": 654918,
+		"nameRu": "СССР. Крушение",
+		"type": "TV_SERIES",
+		"year": "2011",
+		"description": "Восьмисерийная документальная лента рассказывает о том, как и почему перестала существовать великая страна. О внутренних и внешних причинах, предопределивших распад. О перестройке - последнем звене цепи побед и поражений Советского Союза. \n\nРабота над фильмом шла два года. Съемки велись по всему миру - в государствах, которые так или иначе были вовлечены в процесс глобальных перемен второй половины восьмидесятых годов двадцатого века - от Польши и Мальты до Германии и США. \n\nФильм содержит рассказы политиков, дипломатов, военных – тех, кто непосредственно влиял на драматический ход событий, будь то падение Берлинской стены или Беловежские соглашения. Михаил Горбачев, Борис Ельцин, Виктор Черномырдин, Леонид Кравчук, Дмитрий Язов, Джеймс Бейкер, Збигнев Бжезинский, Ганс-Дитрих Геншер - для них разрушение Советского Союза стало частью личной истории. \n\nИнтервью с ключевыми фигурами «великой шахматной доски» и захватывающий перекрестный монтаж, уникальная хроника и новые факты: так от серии к серии возникает все более впечатляющая картина геополитической катастрофы - крушения СССР.",
+		"filmLength": "00:55",
+		"countries": [
+			{
+				"country": "Россия"
+			}
+		],
+		"genres": [
+			{
+				"genre": "документальный"
+			}
+		],
+		"rating": "7.7",
+		"ratingVoteCount": 651,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/654918.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/654918.jpg"
+	},
+	{
+		"filmId": 82364,
+		"nameRu": "Ильза — волчица СС",
+		"nameEn": "Ilsa: She Wolf of the SS",
+		"type": "FILM",
+		"year": "1975",
+		"description": "В некий концентрационный лагерь нацистов, в котором заправляет жестокая фройляйн Ильза, привозят новую порцию заключенных. Над женщинами Ильза с подручными ставят жестокие медицинские опыты, в то время как мужчинам тоже приходится несладко. Ильза каждого из них пробует в постели. Цель Ильзы доказать, что женщины способны выносить больше боли и страданий, чем мужчины, а значит им можно позволить сражаться на фронте.",
+		"filmLength": "01:36",
+		"countries": [
+			{
+				"country": "Канада"
+			}
+		],
+		"genres": [
+			{
+				"genre": "военный"
+			},
+			{
+				"genre": "драма"
+			},
+			{
+				"genre": "триллер"
+			},
+			{
+				"genre": "ужасы"
+			}
+		],
+		"rating": "4.7",
+		"ratingVoteCount": 1559,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/82364.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/82364.jpg"
+	},
+	{
+		"filmId": 4528190,
+		"nameRu": "Магазинчик флирта",
+		"nameEn": "Sseomtaneun pyeonuijeom",
+		"type": "MINI_SERIES",
+		"year": "2021",
+		"description": "Девушка работает продавцом в круглосуточном магазине, где знакомится с четырьмя симпатичными парнями.",
+		"filmLength": "00:12",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "комедия"
+			},
+			{
+				"genre": "мелодрама"
+			}
+		],
+		"rating": "6.7",
+		"ratingVoteCount": 2024,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/4528190.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4528190.jpg"
+	},
+	{
+		"filmId": 1038281,
+		"nameRu": "Круг",
+		"nameEn": "Sseokeul",
+		"type": "TV_SERIES",
+		"year": "2017",
+		"description": "2017 и 2037 год. На Землю прилетает инопланетянин. Пришелец считает, что людям необходимо взять свои эмоции под контроль, иначе у человечества нет будущего.",
+		"filmLength": "01:00",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "драма"
+			},
+			{
+				"genre": "фантастика"
+			}
+		],
+		"rating": "7.5",
+		"ratingVoteCount": 1789,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1038281.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1038281.jpg"
+	},
+	{
+		"filmId": 589738,
+		"nameRu": "Солнечные",
+		"nameEn": "Sseoni",
+		"type": "FILM",
+		"year": "2011",
+		"description": "Жизнь домохозяйки На-ми вполне удалась — она замужем за успешным бизнесменом и воспитывает красавицу-дочь. Однажды в больнице женщина случайно встречается с бывшей одноклассницей, которой жить осталось считанные дни. Та перед смертью хотела бы повидаться с участницами их школьной команды «Солнечные», и На-ми принимается разыскивать подруг, с которыми не виделась много лет.",
+		"filmLength": "02:04",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "драма"
+			},
+			{
+				"genre": "комедия"
+			},
+			{
+				"genre": "мелодрама"
+			}
+		],
+		"rating": "8.1",
+		"ratingVoteCount": 2317,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/589738.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/589738.jpg"
+	},
+	{
+		"filmId": 1111701,
+		"nameRu": "Парень по соседству",
+		"nameEn": "Sseomnam",
+		"type": "TV_SERIES",
+		"year": "2017",
+		"description": "История о двух парнях-соседях, которые начинают жить вместе, и все их принимают за геев.",
+		"filmLength": "00:07",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "комедия"
+			},
+			{
+				"genre": "мелодрама"
+			}
+		],
+		"rating": "8.0",
+		"ratingVoteCount": 838,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1111701.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1111701.jpg"
+	},
+	{
+		"filmId": 4913213,
+		"nameRu": "Летние парни",
+		"nameEn": "Sseommeogaijeu",
+		"type": "TV_SERIES",
+		"year": "2021",
+		"description": "Компания молодёжи решает восстановить старый бар на пляже.",
+		"filmLength": "00:30",
+		"countries": [
+			{
+				"country": "Корея Южная"
+			}
+		],
+		"genres": [
+			{
+				"genre": "мелодрама"
+			},
+			{
+				"genre": "музыка"
+			}
+		],
+		"rating": "null",
+		"ratingVoteCount": 63,
+		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/4913213.jpg",
+		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4913213.jpg"
+	}
+]
