46770d1f0fa3593eef47fc1c4ec131a90331c025
desktop <psomail@yandex.ru>
2024-02-19 23:13:52 +0300

    task11 80

--- task11/LUXERIOUS_FASHION/js/items.js
+++ task11/LUXERIOUS_FASHION/js/items.js
@@ -1,47 +1,65 @@
-const films = JSON.parse(data)
+const items = JSON.parse(data)
 
-const filmsBoxEl = document.querySelector('.cards-box')
-const filmsListEl = document.createElement('ul')
+const itemsSectionEl = document.querySelector('.main_items_section')
+const itemsListEl = document.createElement('ul')
 
-filmsListEl.classList.add('cards')
-filmsBoxEl.appendChild(filmsListEl)
+itemsListEl.classList.add('main_items')
+itemsSectionEl.appendChild(itemsListEl)
 
-films.forEach(film => {
-    const filmEl = document.createElement('li')
-    const filmImgEl = document.createElement('img')
-    const filmInfoEl = document.createElement('div')
-    const filmTitleEl = document.createElement('h2')
-    const filmGenresEl = document.createElement('p')
-    const filmYearEl = document.createElement('p')
-    const filmCountryEl = document.createElement('p')
-    const filmLength = document.createElement('p')
-    const filmDescEl = document.createElement('p')
+items.forEach(item => {
+    const itemEl = document.createElement('li')
+    const productLinkEl= document.createElement('a')
+    const itemImgEl = document.createElement('img')
+    const itemInfoBox = document.createElement('div')
+    const itemTitleEl = document.createElement('h3')
+    const itemPriceEl = document.createElement('p')
+    
 
-    filmEl.classList.add('cards__card')
-    filmInfoEl.classList.add('card__info')
-    filmTitleEl.classList.add('card__title')
-    filmGenresEl.classList.add('card__attr')
-    filmYearEl.classList.add('card__attr')
-    filmCountryEl.classList.add('card__attr')
-    filmLength.classList.add('card__attr')
-    filmDescEl.classList.add('card__desc')
 
-    filmImgEl.src = film.posterUrlPreview
-    filmImgEl.alt = 'poster'
-    filmTitleEl.textContent = film.name
-    filmGenresEl.textContent = film.genres.map(it => it.genre).join(', ')
-    filmYearEl.textContent = film.year
-    filmCountryEl.textContent = film.countries.map(it => it.country).join(', ')
-    filmLength.textContent = film.filmLength
-    filmDescEl.textContent = film.description
 
-    filmEl.appendChild(filmImgEl)
-    filmInfoEl.appendChild(filmTitleEl)
-    filmInfoEl.appendChild(filmGenresEl)
-    filmInfoEl.appendChild(filmYearEl)
-    filmInfoEl.appendChild(filmCountryEl)
-    filmInfoEl.appendChild(filmLength)
-    filmInfoEl.appendChild(filmDescEl)
-    filmEl.appendChild(filmInfoEl)
-    filmsListEl.appendChild(filmEl)
+    itemEl.classList.add('main_items_item')
+
+    itemTitleEl.textContent = item.name
+
+
+    itemEl.appendChild(itemTitleEl)
+    itemsListEl.appendChild(itemEl)
+
+    // const filmEl = document.createElement('li')
+    // const filmImgEl = document.createElement('img')
+    // const filmInfoEl = document.createElement('div')
+    // const filmTitleEl = document.createElement('h2')
+    // const filmGenresEl = document.createElement('p')
+    // const filmYearEl = document.createElement('p')
+    // const filmCountryEl = document.createElement('p')
+    // const filmLength = document.createElement('p')
+    // const filmDescEl = document.createElement('p')
+    //
+    // filmEl.classList.add('cards__card')
+    // filmInfoEl.classList.add('card__info')
+    // filmTitleEl.classList.add('card__title')
+    // filmGenresEl.classList.add('card__attr')
+    // filmYearEl.classList.add('card__attr')
+    // filmCountryEl.classList.add('card__attr')
+    // filmLength.classList.add('card__attr')
+    // filmDescEl.classList.add('card__desc')
+    //
+    // filmImgEl.src = film.posterUrlPreview
+    // filmImgEl.alt = 'poster'
+    // filmTitleEl.textContent = film.name
+    // filmGenresEl.textContent = film.genres.map(it => it.genre).join(', ')
+    // filmYearEl.textContent = film.year
+    // filmCountryEl.textContent = film.countries.map(it => it.country).join(', ')
+    // filmLength.textContent = film.filmLength
+    // filmDescEl.textContent = film.description
+    //
+    // filmEl.appendChild(filmImgEl)
+    // filmInfoEl.appendChild(filmTitleEl)
+    // filmInfoEl.appendChild(filmGenresEl)
+    // filmInfoEl.appendChild(filmYearEl)
+    // filmInfoEl.appendChild(filmCountryEl)
+    // filmInfoEl.appendChild(filmLength)
+    // filmInfoEl.appendChild(filmDescEl)
+    // filmEl.appendChild(filmInfoEl)
+    // filmsListEl.appendChild(filmEl)
 })
--- task11/LUXERIOUS_FASHION/js/json_data.js
+++ task11/LUXERIOUS_FASHION/js/json_data.js
@@ -1,378 +1,68 @@
 const data = `
 [
 	{
-		"filmId": 1040584,
-		"name": "СССР",
-		"type": "TV_SERIES",
-		"year": "2020",
-		"description": "Весна 1981 года. Олимпиада позади, но еще не стерлась со стен олимпийская символика. Спокойное, мирное, сонное время – брежневский застой. Четверо друзей возвращаются из армии, вместе они прошли огонь и воду, нештатные ситуации, спасали друг другу жизнь.",
-		"filmLength": "01:32",
-		"countries": [
-			{
-				"country": "Россия"
-			},
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "мелодрама"
-			}
-		],
-		"rating": "7.7",
-		"ratingVoteCount": 42070,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1040584.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1040584.jpg"
+		"Id": 1,
+		"name": "t-shirt",
+		"brand": "Mango People",
+		"price": 52,
+		"currency": "usd",
+		"image": ".img/rectangle_5_copy_1193.png"
 	},
 	{
-		"filmId": 437392,
-		"name": "Ледяной цветок",
-		"type": "FILM",
-		"year": "2008",
-		"description": "Монарх порабощенного Юаньской империей государства Корё выбрал и воспитал мальчиков для личной стражи, названной Конрёнве. С позволения короля стражники жили во дворце и всегда охраняли своего господина.",
-		"filmLength": "02:12",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "драма"
-			},
-			{
-				"genre": "история"
-			},
-			{
-				"genre": "мелодрама"
-			}
-		],
-		"rating": "7.8",
-		"ratingVoteCount": 9344,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/437392.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/437392.jpg"
+		"Id": 2,
+		"name": "blouse",
+		"brand": "Versace",
+		"price": 70,
+		"currency": "usd",
+		"image": ".img/rectangle_5_copy_1201.png"
 	},
 	{
-		"filmId": 1043837,
-		"name": "Прорвёмся",
-		"type": "TV_SERIES",
-		"year": "2017",
-		"description": "Когда-то в школьные годы Ко Дон-ман занимался тхэквондо и грезил о профессиональном спорте. Сейчас же парень зарабатывает на жизнь участием в боях без правил. Чхве Э-ра надеялась стать диктором на телевидении, но вместо этого работает за информационной стойкой в универмаге. Эта история о том, что судьба часто складывается не так, как того желаешь, однако выбор — сдаваться или сражаться — всегда за тобой.",
-		"filmLength": "01:00",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "комедия"
-			},
-			{
-				"genre": "мелодрама"
-			}
-		],
-		"rating": "8.0",
-		"ratingVoteCount": 17560,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1043837.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1043837.jpg"
+		"Id": 3,
+		"name": "jacket",
+		"brand": "Armani",
+		"price": 85,
+		"currency": "usd",
+		"image": ".img/rectangle_5_copy_1209.png"
 	},
 	{
-		"filmId": 1338307,
-		"name": "Поиск",
-		"type": "TV_SERIES",
-		"year": "2020",
-		"description": "Военно-поисковый отряд в демилитаризованной зоне противостоит неведомому противнику.",
-		"filmLength": "01:00",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "боевик"
-			},
-			{
-				"genre": "триллер"
-			}
-		],
-		"rating": "7.6",
-		"ratingVoteCount": 2938,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1338307.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1338307.jpg"
+		"Id": 4,
+		"name": "dress",
+		"brand": "Mango People",
+		"price": 108,
+		"currency": "usd",
+		"image": ".img/rectangle_5_copy_1240.png"
 	},
 	{
-		"filmId": 1297212,
-		"name": "Бар на колёсах",
-		"type": "TV_SERIES",
-		"year": "2020",
-		"description": "Загадочный бар под вывеской «Twin» материализуется в полночь. Туда захаживают как живые, так и давно почившие личности - общаясь и выпивая с хозяйкой Воль-джу, они залечивают свои душевные раны.",
-		"filmLength": "01:10",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "драма"
-			},
-			{
-				"genre": "комедия"
-			},
-			{
-				"genre": "мелодрама"
-			},
-			{
-				"genre": "фэнтези"
-			}
-		],
-		"rating": "7.9",
-		"ratingVoteCount": 2358,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1297212.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1297212.jpg"
+		"Id": 5,
+		"name": "dress",
+		"brand": "Burberry",
+		"price": 200,
+		"currency": "usd",
+		"image": ".img/rectangle_5_copy_1248.png"
 	},
 	{
-		"filmId": 1272433,
-		"name": "Дневник психопата",
-		"type": "TV_SERIES",
-		"year": "2019",
-		"description": "К скромному служащему брокерской конторы Юк Тон-щику в руки случайно попадает дневник серийного убийцы, где подробно описаны его преступления. После этого парень из-за несчастного случая теряет память и решает, что он сам и есть маньяк. С этого момента Тон-щик начинает меняться. Полицейская Щим Бо-гён решает использовать эту возможность, чтобы поймать настоящего убийцу.",
-		"filmLength": "01:20",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "детектив"
-			},
-			{
-				"genre": "комедия"
-			},
-			{
-				"genre": "криминал"
-			},
-			{
-				"genre": "триллер"
-			}
-		],
-		"rating": "7.8",
-		"ratingVoteCount": 9029,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1272433.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1272433.jpg"
+		"Id": 6,
+		"name": "suit",
+		"brand": "Prada",
+		"price": 111,
+		"currency": "usd",
+		"image": ".img/rectangle_5_copy_1256.png"
 	},
 	{
-		"filmId": 570147,
-		"name": "Назад в СССР",
-		"type": "MINI_SERIES",
-		"year": "2010",
-		"description": "Преуспевающий бизнесмен Антон переживает кризис среднего возраста. Он много выпивает и по совету близкого друга начинает посещать общество анонимных алкоголиков. Там мужчина знакомится с человеком, называющим себя Сталкером, который предлагает приключение и ощущения, которых он никогда не испытывал. Антон соглашается и внезапно понимает, что оказался в 1975 году.",
-		"filmLength": "00:47",
-		"countries": [
-			{
-				"country": "Россия"
-			}
-		],
-		"genres": [
-			{
-				"genre": "мелодрама"
-			},
-			{
-				"genre": "фантастика"
-			}
-		],
-		"rating": "6.8",
-		"ratingVoteCount": 12063,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/570147.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/570147.jpg"
+		"Id": 7,
+		"name": "trousers",
+		"brand": "Fashion Forward",
+		"price": 59,
+		"currency": "usd",
+		"image": ".img/rectangle_5_copy_1264.png"
 	},
 	{
-		"filmId": 984253,
-		"name": "Давай сражаться, призрак",
-		"type": "TV_SERIES",
-		"year": "2016",
-		"description": "Пак обладает сверхъестественной способностью не только видеть призраков, но и изгонять их. Поэтому с удовольствием оказывает помощь тем, кто нуждается в услугах экзорциста. Но однажды он сталкивается с призраком школьницы, и во время исполнения обряда случайно целует её, да так, что искры летят.",
-		"filmLength": "01:00",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "комедия"
-			},
-			{
-				"genre": "мелодрама"
-			},
-			{
-				"genre": "фэнтези"
-			}
-		],
-		"rating": "7.8",
-		"ratingVoteCount": 18506,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/984253.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/984253.jpg"
-	},
-	{
-		"filmId": 996611,
-		"name": "Демон",
-		"type": "TV_SERIES",
-		"year": "2016",
-		"description": "Бессмертный демон токкэби много лет живет среди смертных и порядком устал от жизни. Но если ты волшебное существо, есть лишь один способ поставить точку и покинуть бренный мир — жениться на смертной. Избранницей демона становится девушка, которая может видеть призраков. А ангел смерти, чья задача провожать души умерших в загробный мир, тем временем потерял память.",
-		"filmLength": "01:15",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "драма"
-			},
-			{
-				"genre": "комедия"
-			},
-			{
-				"genre": "мелодрама"
-			},
-			{
-				"genre": "фэнтези"
-			}
-		],
-		"rating": "8.5",
-		"ratingVoteCount": 150172,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/996611.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/996611.jpg"
-	},
-	{
-		"filmId": 654918,
-		"name": "СССР. Крушение",
-		"type": "TV_SERIES",
-		"year": "2011",
-		"description": "Восьмисерийная документальная лента рассказывает о том, как и почему перестала существовать великая страна. О внутренних и внешних причинах, предопределивших распад.",
-		"filmLength": "00:55",
-		"countries": [
-			{
-				"country": "Россия"
-			}
-		],
-		"genres": [
-			{
-				"genre": "документальный"
-			}
-		],
-		"rating": "7.7",
-		"ratingVoteCount": 651,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/654918.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/654918.jpg"
-	},
-	{
-		"filmId": 4528190,
-		"name": "Магазинчик флирта",
-		"type": "MINI_SERIES",
-		"year": "2021",
-		"description": "Девушка работает продавцом в круглосуточном магазине, где знакомится с четырьмя симпатичными парнями.",
-		"filmLength": "00:12",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "комедия"
-			},
-			{
-				"genre": "мелодрама"
-			}
-		],
-		"rating": "6.7",
-		"ratingVoteCount": 2024,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/4528190.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4528190.jpg"
-	},
-	{
-		"filmId": 1038281,
-		"name": "Круг",
-		"type": "TV_SERIES",
-		"year": "2017",
-		"description": "2017 и 2037 год. На Землю прилетает инопланетянин. Пришелец считает, что людям необходимо взять свои эмоции под контроль, иначе у человечества нет будущего.",
-		"filmLength": "01:00",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "драма"
-			},
-			{
-				"genre": "фантастика"
-			}
-		],
-		"rating": "7.5",
-		"ratingVoteCount": 1789,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1038281.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1038281.jpg"
-	},
-	{
-		"filmId": 589738,
-		"name": "Солнечные",
-		"type": "FILM",
-		"year": "2011",
-		"description": "Жизнь домохозяйки На-ми вполне удалась — она замужем за успешным бизнесменом и воспитывает красавицу-дочь. Однажды в больнице женщина случайно встречается с бывшей одноклассницей, которой жить осталось считанные дни. Та перед смертью хотела бы повидаться с участницами их школьной команды «Солнечные», и На-ми принимается разыскивать подруг, с которыми не виделась много лет.",
-		"filmLength": "02:04",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "драма"
-			},
-			{
-				"genre": "комедия"
-			},
-			{
-				"genre": "мелодрама"
-			}
-		],
-		"rating": "8.1",
-		"ratingVoteCount": 2317,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/589738.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/589738.jpg"
-	},
-	{
-		"filmId": 1111701,
-		"name": "Парень по соседству",
-		"type": "TV_SERIES",
-		"year": "2017",
-		"description": "История о двух парнях-соседях, которые начинают жить вместе, и все их принимают за геев.",
-		"filmLength": "00:07",
-		"countries": [
-			{
-				"country": "Корея Южная"
-			}
-		],
-		"genres": [
-			{
-				"genre": "комедия"
-			},
-			{
-				"genre": "мелодрама"
-			}
-		],
-		"rating": "8.0",
-		"ratingVoteCount": 838,
-		"posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1111701.jpg",
-		"posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1111701.jpg"
+		"Id": 8,
+		"name": "sweatshirt",
+		"brand": "Dreamy Days",
+		"price": 20,
+		"currency": "usd",
+		"image": ".img/rectangle_5_copy_1272.png"
 	}
 ]
 `
